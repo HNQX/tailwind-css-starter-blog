1a4f26c5e7c69cdbbb5cd31073b754fd88e90222
han <473947605@qq.com>
2023-11-09 02:01:17 +0000

    1

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,8 +2,14 @@ const projectsData = [
   {
     title: 'XiaoPang',
     description: `临时传递你的文件&文档`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/xiaopang.png',
     href: 'https://52152121.xyz:802',
+  },
+  {
+    title: 'Shortiny',
+    description: `短网址&管理`,
+    imgSrc: '/static/images/shortiny.png',
+    href: 'https://url3.fun',
   },
 ]
 
